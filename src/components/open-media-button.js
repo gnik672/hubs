d489bca03a98e123972a4e8cb4b7307e61655945
kontopoulos <kontopoulos@synelixis.com>
2025-06-17 11:30:32 +0000

    new direction links for new rooms

--- src/components/open-media-button.js
+++ src/components/open-media-button.js
@@ -63,7 +63,7 @@ AFRAME.registerComponent("open-media-button", {
       const username = window?.APP?.store?.state?.profile?.displayName;
 
 
-// from lobby production
+// from lobby  session 1
 
 if(window.APP.hub.hub_id ===  "QBomv74" ||
 window.APP.hub.hub_id ===  "DdXYP6a" || 
@@ -75,43 +75,189 @@ window.APP.hub.hub_id ===  "zts4Mhv" ||
      window.APP.hub.hub_id ===  "iuUMSDP"  || 
      window.APP.hub.hub_id ===  "ZwawSJC" || 
      window.APP.hub.hub_id ===  "pe5ngbm" 
-     ) {
+     ) { 
+      const hubId = "TfszbgC"; // Your room ID
+      const waypoint = null;   // Or a specific waypoint ID if needed
+      changeHub(hubId, true, waypoint);
+      return;  
 
-      if (username === "user-a" || username === "user-b" ){
+     }
+
+     if(window.APP.hub.hub_id ===  "TfszbgC"  
+     ) { 
+      const hubId = "Lf5offt"; // Your room ID
+      const waypoint = null;   // Or a specific waypoint ID if needed
+      changeHub(hubId, true, waypoint);
+      return;  
+
+     }
+
+     if(window.APP.hub.hub_id ===  "Lf5offt"  
+     ) { 
+      if (username === "user-1" || username === "user-2" ){
         const hubId = "DSinvBh"; // Your room ID
        const waypoint = null;   // Or a specific waypoint ID if needed
        changeHub(hubId, true, waypoint);
        return; 
+
      }
-     if (username === "user-c" || username === "user-d" ){
-     const hubId = "vCAqAvY"; // Your room ID
+
+     if (username === "user-3" || username === "user-4" ){
+      const hubId = "vCAqAvY"; // Your room ID
+       const waypoint = null;   // Or a specific waypoint ID if needed
+        changeHub(hubId, true, waypoint);
+       return;    }
+       if (username === "user-5" || username === "user-6" ){
+        const hubId = "MZbYQFN"; // Your room ID
+         const waypoint = null;   // Or a specific waypoint ID if needed
+         changeHub(hubId, true, waypoint);
+         return;    }
+         if (username === "user-7" || username === "user-8" ){
+          const hubId = "DzM288m"; // Your room ID
+           const waypoint = null;   // Or a specific waypoint ID if needed
+           changeHub(hubId, true, waypoint);
+           return;    }   
+           if (username === "user-9" || username === "user-10" ){
+            const hubId = "DzM288m"; // Your room ID
+             const waypoint = null;   // Or a specific waypoint ID if needed
+             changeHub(hubId, true, waypoint);
+             return;    }  
+                          
+    
+   }
+  
+ 
+
+// from lobby  session 2
+
+if(window.APP.hub.hub_id ===  "oLohVr8" ||
+window.APP.hub.hub_id ===  "LB93AmU" || 
+window.APP.hub.hub_id ===  "XQPkmkk" ||
+  window.APP.hub.hub_id ===  "9f3jrjg" ||
+  window.APP.hub.hub_id ===  "XfETMdM" || 
+   window.APP.hub.hub_id ===  "zKM5ykf" ||
+    window.APP.hub.hub_id ===  "PKXyPyW" || 
+     window.APP.hub.hub_id ===  "Db39mTE"  || 
+     window.APP.hub.hub_id ===  "znaYFyF" || 
+     window.APP.hub.hub_id ===  "6RobLnj" 
+     ) { 
+      const hubId = "CDvPjH9"; // Your room ID
       const waypoint = null;   // Or a specific waypoint ID if needed
       changeHub(hubId, true, waypoint);
-      return;    }
-      if (username === "user-e" || username === "user-f" ){
-       const hubId = "MZbYQFN"; // Your room ID
-        const waypoint = null;   // Or a specific waypoint ID if needed
-        changeHub(hubId, true, waypoint);
-        return;    }
-        if (username === "user-g" || username === "user-h" ){
-         const hubId = "DzM288m"; // Your room ID
-          const waypoint = null;   // Or a specific waypoint ID if needed
-          changeHub(hubId, true, waypoint);
-          return;    }
-          if (username === "user-i" || username === "user-j" ){
-           const hubId = "zKapQ9v"; // Your room ID
-            const waypoint = null;   // Or a specific waypoint ID if needed
-            changeHub(hubId, true, waypoint);
-            return;    }
-    
+      return;  
 
      }
 
-                           
+     if(window.APP.hub.hub_id ===  "CDvPjH9"  
+     ) { 
+      const hubId = "5tsdP2w"; // Your room ID
+      const waypoint = null;   // Or a specific waypoint ID if needed
+      changeHub(hubId, true, waypoint);
+      return;  
+
+     }
+
+     if(window.APP.hub.hub_id ===  "5tsdP2w"  
+     ) { 
+      if (username === "user-11" || username === "user-12" ){
+        const hubId = "a9a9C6U"; // Your room ID
+       const waypoint = null;   // Or a specific waypoint ID if needed
+       changeHub(hubId, true, waypoint);
+       return; 
+
+     }
+
+     if (username === "user-13" || username === "user-14" ){
+      const hubId = "5pUKqhb"; // Your room ID
+       const waypoint = null;   // Or a specific waypoint ID if needed
+        changeHub(hubId, true, waypoint);
+       return;    }
+       if (username === "user-15" || username === "user-16" ){
+        const hubId = "sY6HJki"; // Your room ID
+         const waypoint = null;   // Or a specific waypoint ID if needed
+         changeHub(hubId, true, waypoint);
+         return;    }
+         if (username === "user-17" || username === "user-18" ){
+          const hubId = "9wwPDe8"; // Your room ID
+           const waypoint = null;   // Or a specific waypoint ID if needed
+           changeHub(hubId, true, waypoint);
+           return;    }   
+           if (username === "user-19" || username === "user-20" ){
+            const hubId = "hBEMigy"; // Your room ID
+             const waypoint = null;   // Or a specific waypoint ID if needed
+             changeHub(hubId, true, waypoint);
+             return;    }  
                           
-                       
-                       
-                           
+    
+   }
+
+
+
+
+// from lobby  session 3
+
+if(window.APP.hub.hub_id ===  "24DXNLW" ||
+window.APP.hub.hub_id ===  "oqbV9pN" || 
+window.APP.hub.hub_id ===  "xHduK5S" ||
+  window.APP.hub.hub_id ===  "ZdhFkXn" ||
+  window.APP.hub.hub_id ===  "kz2E9Vi" || 
+   window.APP.hub.hub_id ===  "3g9FXe7" ||
+    window.APP.hub.hub_id ===  "FMnPety" || 
+     window.APP.hub.hub_id ===  "HzFHXBA"  || 
+     window.APP.hub.hub_id ===  "sgvW4vB" || 
+     window.APP.hub.hub_id ===  "pir7nGq" 
+     ) { 
+      const hubId = "uYLmstU"; // Your room ID
+      const waypoint = null;   // Or a specific waypoint ID if needed
+      changeHub(hubId, true, waypoint);
+      return;  
+
+     }
+
+     if(window.APP.hub.hub_id ===  "uYLmstU"  
+     ) { 
+      const hubId = "wmJrZRW"; // Your room ID
+      const waypoint = null;   // Or a specific waypoint ID if needed
+      changeHub(hubId, true, waypoint);
+      return;  
+
+     }
+
+     if(window.APP.hub.hub_id ===  "wmJrZRW"  
+     ) { 
+      if (username === "user-21" || username === "user-22" ){
+        const hubId = "xKkxuwL"; // Your room ID
+       const waypoint = null;   // Or a specific waypoint ID if needed
+       changeHub(hubId, true, waypoint);
+       return; 
+
+     }
+
+     if (username === "user-23" || username === "user-24" ){
+      const hubId = "ngXS8Dg"; // Your room ID
+       const waypoint = null;   // Or a specific waypoint ID if needed
+        changeHub(hubId, true, waypoint);
+       return;    }
+       if (username === "user-25" || username === "user-26" ){
+        const hubId = "Eo8WYHm"; // Your room ID
+         const waypoint = null;   // Or a specific waypoint ID if needed
+         changeHub(hubId, true, waypoint);
+         return;    }
+         if (username === "user-27" || username === "user-28" ){
+          const hubId = "P34XsVp"; // Your room ID
+           const waypoint = null;   // Or a specific waypoint ID if needed
+           changeHub(hubId, true, waypoint);
+           return;    }   
+           if (username === "user-29" || username === "user-30" ){
+            const hubId = "6KRbEwB"; // Your room ID
+             const waypoint = null;   // Or a specific waypoint ID if needed
+             changeHub(hubId, true, waypoint);
+             return;    }  
+                          
+    
+   }
+     
+     
                          
                          
                            
@@ -150,6 +296,79 @@ window.APP.hub.hub_id ===  "zts4Mhv" ||
        
   
    }  
+
+
+
+// from lobby  session 4
+
+if(window.APP.hub.hub_id ===  "e9WjxAt" ||
+window.APP.hub.hub_id ===  "T88zL9U" || 
+window.APP.hub.hub_id ===  "J2LSN4u" ||
+  window.APP.hub.hub_id ===  "xvxN2NR" ||
+  window.APP.hub.hub_id ===  "7dJQzWL" || 
+   window.APP.hub.hub_id ===  "5tkKH9e" ||
+    window.APP.hub.hub_id ===  "NDe82ac" || 
+     window.APP.hub.hub_id ===  "p9krCnq"  || 
+     window.APP.hub.hub_id ===  "dghjLoq" || 
+     window.APP.hub.hub_id ===  "iFtyp99" 
+     ) { 
+      const hubId = "4P2AN2P"; // Your room ID
+      const waypoint = null;   // Or a specific waypoint ID if needed
+      changeHub(hubId, true, waypoint);
+      return;  
+
+     }
+
+     if(window.APP.hub.hub_id ===  "4P2AN2P"  
+     ) { 
+      const hubId = "JqLXc2T"; // Your room ID
+      const waypoint = null;   // Or a specific waypoint ID if needed
+      changeHub(hubId, true, waypoint);
+      return;  
+
+     }
+
+     if(window.APP.hub.hub_id ===  "JqLXc2T"  
+     ) { 
+      if (username === "user-31" || username === "user-32" ){
+        const hubId = "VXyBdWu"; // Your room ID
+       const waypoint = null;   // Or a specific waypoint ID if needed
+       changeHub(hubId, true, waypoint);
+       return; 
+
+     }
+
+     if (username === "user-33" || username === "user-34" ){
+      const hubId = "Arvck4f"; // Your room ID
+       const waypoint = null;   // Or a specific waypoint ID if needed
+        changeHub(hubId, true, waypoint);
+       return;    }
+       if (username === "user-35" || username === "user-36" ){
+        const hubId = "aFNyuny"; // Your room ID
+         const waypoint = null;   // Or a specific waypoint ID if needed
+         changeHub(hubId, true, waypoint);
+         return;    }
+         if (username === "user-37" || username === "user-38" ){
+          const hubId = "fkLMpzR"; // Your room ID
+           const waypoint = null;   // Or a specific waypoint ID if needed
+           changeHub(hubId, true, waypoint);
+           return;    }   
+           if (username === "user-39" || username === "user-40" ){
+            const hubId = "Wrof4qm"; // Your room ID
+             const waypoint = null;   // Or a specific waypoint ID if needed
+             changeHub(hubId, true, waypoint);
+             return;    }  
+                          
+    
+   }
+     
+     
+
+
+
+
+
+
   
    if(window.APP.hub.hub_id ===  "H7yKnL7"){
     // Force redirect if this is the specific room we want
