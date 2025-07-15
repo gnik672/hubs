84056aa1bc40aca465249024eda815881b6cbba3
kontopoulos <kontopoulos@synelixis.com>
2025-07-15 09:07:44 +0000

    correction in the previous

--- src/react-components/ui-root.js
+++ src/react-components/ui-root.js
@@ -1410,7 +1410,7 @@ class UIRoot extends Component {
                     {!this.state.dialog && renderEntryFlow ? entryDialog : undefined}
                     {!this.props.selectedObject && <CompactMoreMenuButton />}
                     {(!this.props.selectedObject ||
-                      (this.props.breakpoint !== "sm" && this.props.breakpoint !== "md")) && window.APP?.store?.state?.profile?.displayName.toLowerCase().includes("presenter") && (
+                      (this.props.breakpoint !== "sm" && this.props.breakpoint !== "md"))  && (
                     
                       <ContentMenu>
                         {/* {showObjectList && (
